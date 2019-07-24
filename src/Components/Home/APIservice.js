47ac75b38f3e3ec71620fc3f1deb3d03f018aa80
ganjali <gaddam.anjali@oracle.com>
2019-07-24 06:45:24 +0530

    modify API requests, return the value using Promises and reject if it catches any error

--- src/Components/Home/APIservice.js
+++ src/Components/Home/APIservice.js
@@ -1,21 +1,38 @@
 const axios = require('axios');
 
-export const getNotes = async (user) => {
-  const notes = await axios.get(`${process.env.REACT_APP_API_HOST}/${user}/notes`);
-  return notes;
-};
 
-export const deleteNote = async (username, noteid) => {
-  const deletedNote = await axios.delete(`${process.env.REACT_APP_API_HOST}/${username}/notes/${noteid}`);
-  return deletedNote;
-};
+export const getNotes = user => new Promise((async (resolve, reject) => {
+  try {
+    const notes = await axios.get(`${process.env.REACT_APP_API_HOST}/${user}/notes`);
+    resolve(notes);
+  } catch (e) {
+    reject(e);
+  }
+}));
 
-export const saveNote = async (username, note) => {
-  const savedNote = await axios.post(`${process.env.REACT_APP_API_HOST}/${username}/notes`, note);
-  return savedNote;
-};
+export const saveNote = (username, note) => new Promise((async (resolve, reject) => {
+  try {
+    const savedNote = await axios.post(`${process.env.REACT_APP_API_HOST}/${username}/notes`, note);
+    resolve(savedNote);
+  } catch (e) {
+    reject(e);
+  }
+}));
 
-export const updateNote = async (username, note) => {
-  const updatedNote = await axios.post(`${process.env.REACT_APP_API_HOST}/${username}/notes/${note.id}`, note);
-  return updatedNote;
-};
+export const updateNote = (username, note) => new Promise((async (resolve, reject) => {
+  try {
+    const updatedNote = await axios.post(`${process.env.REACT_APP_API_HOST}/${username}/notes/${note.id}`, note);
+    resolve(updatedNote);
+  } catch (e) {
+    reject(e);
+  }
+}));
+
+export const deleteNote = (username, noteid) => new Promise((async (resolve, reject) => {
+  try {
+    const deletedNote = await axios.delete(`${process.env.REACT_APP_API_HOST}/${username}/notes/${noteid}`);
+    resolve(deletedNote);
+  } catch (e) {
+    reject(e);
+  }
+}));
--- src/Components/Login/APIservice.js
+++ src/Components/Login/APIservice.js
@@ -1,11 +1,15 @@
 const axios = require('axios');
 
-const isValidCredentials = async (username, password) => {
-  const callStatus = await axios.post(`${process.env.REACT_APP_API_HOST}/login`, {
-    username,
-    password,
-  }).catch(error => error);
-  return callStatus;
-};
+const isValidCredentials = (username, password) => new Promise((async (resolve, reject) => {
+  try {
+    const callStatus = await axios.post(`${process.env.REACT_APP_API_HOST}/login`, {
+      username,
+      password,
+    });
+    resolve(callStatus);
+  } catch (e) {
+    reject(e);
+  }
+}));
 
 export default isValidCredentials;
